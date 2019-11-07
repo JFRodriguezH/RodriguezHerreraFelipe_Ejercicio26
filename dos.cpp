8ff3a8de6ad4bf4884a8c658df231a0903150377
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-11-07 18:39:27 +0000

    Se agregaron los comentarios al codigo

--- dos.cpp
+++ dos.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 float * lee(string filename, int *n_points);
-int mult(float *x, float *y, int n_x, string file_name);
+void mult(float *x, float *y, int n_x, string file_name);
 
 int main(){
+    /*Inicializa los arreglos y las variables*/
     float *x=NULL;
     float *y=NULL;
     int n_x=0;
     int n_y=0;
-
+    /*Lee los archivos y lo guarda en los arreglos*/
     x = lee("valores_x.txt", &n_x);
     y = lee("valores_y.txt", &n_y);
+    /*El nombre del archivo que se va a crear*/
     string file_name = "multiplicados.dat";
-    
+    /*Se crea el archivo*/
     mult(x, y, n_x, file_name);
   
     return 0;
@@ -54,17 +56,19 @@ float * lee(string filename, int *n_points){
     return array;
 }
 
-int mult(float *x, float *y, int n_x, string file_name){
+void mult(float *x, float *y, int n_x, string file_name){
+    /*Crea el archivo y lo abre*/
     ofstream outfile;
     outfile.open(file_name);
     double a;
     double b;
     cout << "Escribiendo en " << file_name << endl; 
     for (int i=0; i < n_x; i++){
+        /*Guarda los valores que lee de los arreglos de entrada*/
         a = x[i];
         b = y[i];
+        /*Escribe el produco en el archivo*/
         outfile << (a*b)  << endl;
     }
     outfile.close(); 
-    return 0;
 }
