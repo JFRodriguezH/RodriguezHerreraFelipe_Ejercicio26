cdce3cfe69ef323465db643bc05ddd9e8636d54d
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-11-07 18:41:44 +0000

    Se agregaron los comentarios al codigo

--- uno.cpp
+++ uno.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
 int main(){
+    /*Inicializa las variables*/
     int* a = NULL;
     int x;
+    /*Pregunta por la terminal el tamaño del arreglo*/
     std::cout<<"Escriba el tamaño del arreglo: "<<std::endl;
     std::cin>>x;
+    /*Guarda el espacio en la memoria*/
     a = new int[x];
+    /*Escribe en el arreglo*/
     std::cout<<"Su arreglo es: "<<std::endl;
     std::cout<<0<<std::endl;
     for(int i=1; i<x;i++){
         a[i]=1;
+        /*Calcula el factorial de la posición*/
         for(int j=i;j>0;j--){
            a[i]= a[i]*j; 
         }
         std::cout<<a[i]<<std::endl;
     }
+    /*Libera el espacio en la memora*/
     delete [] a;
     return 0;   
 }
